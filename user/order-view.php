ba96ff072682a4315032adfd39aef868288b5198
Yash Shinde <yashvshinde75@gmail.com>
2025-12-30 15:11:10 +0530

    Fix session/auth include paths in order view

--- user/order-view.php
+++ user/order-view.php
@@ -1,6 +1,10 @@
 <?php
-require_once __DIR__ . "/session.php";
-require_once '../config.php';
+require_once __DIR__ . "/../session.php";
+require_once __DIR__ . "/../auth.php";
+require_login();
+
+require_once __DIR__ . "/../config.php";
+
 
 if (!isset($_SESSION['user_id'])) {
     header("Location: login.php");
